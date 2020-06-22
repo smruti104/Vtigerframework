7bce100ac532399ff0d25e0bc5037a16a15701bd
smruti <smruti@smruti-PC>
2020-06-23 02:15:12 +0530

    added new class

--- VtigerFramework/src/test/java/com/vtiger/TestScripts/CreateTest.java
+++ VtigerFramework/src/test/java/com/vtiger/TestScripts/CreateTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 public class CreateTest {
 	@Test
 	public void CreateTest1() {
-		System.out.println("goodggg nyt");
+		System.out.println("good morning");
+		System.out.println("demo exstance");
+		
 	}
 }

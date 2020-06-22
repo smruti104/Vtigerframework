22509c8c6b35cba6476d84ec2b2727e656e9f0c9
smruti <smruti@smruti-PC>
2020-06-23 00:38:56 +0530

    updated file done by smruti

---
+++ VtigerFramework/src/test/java/com/vtiger/TestScripts/CreateTest.java
@@ -0,0 +1,10 @@
+package com.vtiger.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class CreateTest {
+	@Test
+	public void CreateTest() {
+		System.out.println("good morning");
+	}
+}

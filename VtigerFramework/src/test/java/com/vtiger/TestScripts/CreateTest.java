00d873cd167cc44b7225d0195d50bf52e96bc57f
smruti <smruti@smruti-PC>
2020-06-24 17:48:10 +0530

    updated

--- VtigerFramework/src/test/java/com/vtiger/TestScripts/CreateTest.java
+++ VtigerFramework/src/test/java/com/vtiger/TestScripts/CreateTest.java
@@ -7,6 +7,7 @@ public class CreateTest {
 	public void CreateTest1() {
 		System.out.println("good morning");
 		System.out.println("demo exstance");
+		System.out.println("hii");
 		System.out.println("good afternoon");
 		System.out.println("good evening");
 	}
